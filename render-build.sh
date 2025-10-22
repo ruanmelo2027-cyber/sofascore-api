b55103410230821a1b057cbf557f838b76ba6c02
ominoN@03021003 <ruan.melo.2025@gmail.com>
2025-10-22 13:09:11 -0300

    Corrigido build e removido render-build.sh

--- render-build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# Instala dependências do projeto
-npm install
-
-# Instala Playwright (com permissão explícita)
-npx playwright install chromium --with-deps || echo "Playwright já instalado"
